6ef61d438e0c7a30d8d6db371e4f0474f1c53962
Lucia Viazzo <viazzo94@gmail.com>
2024-06-18 23:15:41 -0300

    Format docuement

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { PAINTINGS } from './data.js'
+import { paintingRoutes } from './routes/paintingRoutes.js'
 
 dotenv.config()
 
 const SERVER_PORT = process.env.SERVER_PORT || 3003
 
 const app = express()
-//app.use(express.json())
+app.use(express.json())
+
+app.use('/api', paintingRoutes(PAINTINGS))
 
 //app.use('/api', bookRoutes(), userRouter)
 //app.use(errorHandler)
--- routes/paintingRoutes.js
+++ routes/paintingRoutes.js
@@ -1,22 +1,23 @@
 //Guarda los metodos
 
-import { Router } from 'express' //Router nque me bridna express
+import { Router } from 'express' //Router que me bridna express
 import { paintingsController } from '../controllers/paintingsController.js'
 import { schemaValidator } from '../middlewares/validations.js'
 import { bodyPintingSchema, updatePaintingSchema } from '../schemas/paintingsSchemas.js'
 
-export const paintingRouter = () => {
-  const paintingRouter = Router()
+export const paintingRoutes = () => {
+  const paintingRoutes = Router()
   const { getPainting, createPainting, getPaintingById, deleteById, updateById } = paintingsController()
 
-  paintingRouter.route('/paintings')
+  //Agrupo los request de acuerdo a las rutas
+  paintingRoutes.route('/paintings')
     .get(getPainting)
     .post(schemaValidator(bodyPintingSchema), createPainting)
 
-    paintingRouter.route('/paintings/:id')
+  paintingRoutes.route('/paintings/:id')
     .get(getPaintingById)
     .delete(deleteById)
     .patch(schemaValidator(updatePaintingSchema), updateById)
 
-  return paintingRouter
+  return paintingRoutes
 }
