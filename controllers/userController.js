fa621cc3edf54a4847e4813282ebbd0355e91b29
Lucia Viazzo <viazzo94@gmail.com>
2024-07-03 20:34:09 -0300

    user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,126 +1,122 @@
-import httpStatus from '../helpers/httpStatus.js'
+import httpStatus from "../helpers/httpStatus.js";
+import { generateToken, verifyToken } from "../utils/tokenManagement.js";
+import { encrypt, verified } from "../utils/bcrypt.js";
+import { PrismaClient } from "@prisma/client";
 
-import { generateToken, verifyToken } from '../utils/tokenManagement.js'
-import { encrypt, verified } from '../utils/bcrypt.js'
-
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const userController = () => {
   const register = async (request, response, next) => {
-    const newUser = request.body
-    const hashedPassword = await encrypt(newUser.password)
-    newUser.password = hashedPassword
+    const newUser = request.body;
+    const hashedPassword = await encrypt(newUser.password);
+    newUser.password = hashedPassword;
 
     try {
       const createdUser = await prisma.user.create({
-        data: newUser
-      })
+        data: newUser,
+      });
 
       const responseFormat = {
         data: createdUser,
-        message: 'User created successfully'
-      }
+        message: "User created successfully",
+      };
 
-      return response.status(httpStatus.CREATED).json(responseFormat)
+      return response.status(httpStatus.CREATED).json(responseFormat);
     } catch (error) {
-      next(error)
+      next(error);
     } finally {
-      await prisma.$disconnect()
+      await prisma.$disconnect();
     }
-  }
+  };
 
   const login = async (request, response, next) => {
-    const { email, password } = request.body
+    const { email, password } = request.body;
 
     try {
       const user = await prisma.user.findUnique({
         where: {
-          email
-        }
-      })
+          email,
+        },
+      });
 
       if (!user) {
         return response.status(httpStatus.NOT_FOUND).json({
-          message: 'Invalid credentials'
-        })
+          message: "Invalid credentials",
+        });
       }
 
-      const isPasswordValid = await verified(password, user.password)
+      const isPasswordValid = await verified(password, user.password);
 
       if (!isPasswordValid) {
         return response.status(httpStatus.UNAUTHORIZED).json({
-          message: 'Invalid credentials'
-        })
+          message: "Invalid credentials",
+        });
       }
 
       //* AQUI TENGO QUE GENERAR EL TOKEN
 
-      const token = generateToken({data:{ email, role: user.role }})
+      const token = generateToken({ data: { email, role: user.role } });
       const refreshToken = generateToken({
-        data:{ email, role: user.role },
+        data: { email, role: user.role },
         isRefresh: true,
-        expiresIn: '7d'
-      })
-
+        expiresIn: "7d",
+      });
 
       return response.status(httpStatus.OK).json({
-        message: 'Login successful',
+        message: "Login successful",
         token,
-        refreshToken
-      })
+        refreshToken,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     } finally {
-      await prisma.$disconnect()
+      await prisma.$disconnect();
     }
-  }
+  };
 
   const refreshToken = async (request, response, next) => {
-    const { refreshToken } = request.body
+    const { refreshToken } = request.body;
 
     try {
-      const { role, email } = verifyToken(refreshToken, true)
+      const { role, email } = verifyToken(refreshToken, true);
       const token = generateToken({
-        data:{ email, role, message: 'fressssco' }
-      })
+        data: { email, role, message: "fressssco" },
+      });
 
       return response.status(httpStatus.OK).json({
         success: true,
-        token
-      })
+        token,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     }
-  }
+  };
 
   const profile = async (request, response, next) => {
-    const { id } = request.params
-    const userId = Number(id)
+    const { id } = request.params;
+    const userId = Number(id);
 
     try {
       const user = await prisma.user.findUnique({
         where: {
-          id: userId
-        }
-      })
+          id: userId,
+        },
+      });
 
       return response.status(httpStatus.OK).json({
-        data: user
-      })
+        data: user,
+      });
     } catch (error) {
-      next(error)
+      next(error);
     } finally {
-      await prisma.$disconnect()
+      await prisma.$disconnect();
     }
-  }
-
-  
+  };
 
   return {
     register,
     login,
     profile,
-    refreshToken
-  }
-}
+    refreshToken,
+  };
+};
