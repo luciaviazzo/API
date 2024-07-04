131f77288da7780ea8637f6530a03ea5838d4e24
Lucia Viazzo <viazzo94@gmail.com>
2024-07-04 11:28:46 -0300

    resolute connection

--- prisma/migrations/20240704142412_init/migration.sql
+++ prisma/migrations/20240704142412_init/migration.sql
@@ -1,4 +1,17 @@
 -- CreateTable
+CREATE TABLE "paintings" (
+    "id" SERIAL NOT NULL,
+    "title" VARCHAR(80) NOT NULL,
+    "author" VARCHAR(80) NOT NULL,
+    "description" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "created at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "paintings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "users" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
